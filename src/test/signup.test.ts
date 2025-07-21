adc408d6d8885073abb329b57aee89368e37c1c0
Renan1fps <renan.sergipe68@gmail.com>
2025-07-21 09:12:50 -0300

    test: ensure account creation with a valid email

--- src/test/signup.test.ts
+++ src/test/signup.test.ts
@@ -33,4 +33,17 @@ describe('Signup [integration]', () => {
         expect(responseSignup.status).toBe(422);
         expect(outputSignup.message).toBe('Invalid name');
     });
+
+    test('Should return an error if the email is invalid', async() => {
+        const inputSignup = {
+            name: 'Jhon Doe',
+            email: 'jhondoe',
+            document: '60993883893',
+            password: 'Test.1234',
+        }
+        const responseSignup = await axios.post('http://localhost:3025/signup', inputSignup);
+        const outputSignup = responseSignup.data;
+        expect(responseSignup.status).toBe(422);
+        expect(outputSignup.message).toBe('Invalid email');
+    });
 });
