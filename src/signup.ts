20c11e9cb016520df08163b140da20059055119c
Renan1fps <renan.sergipe68@gmail.com>
2025-07-21 08:53:16 -0300

    feat: save account and get account saved

--- src/signup.ts
+++ src/signup.ts
@@ -2,9 +2,27 @@ import { randomUUID } from "crypto";
 import express, { Request, Response } from "express";
 
 const app = express();
+app.use(express.json())
+
+const accounts: any = [];
 
 app.post('/signup', async(req: Request, res: Response) => {
-    res.json({ accountId: randomUUID() })
+    const body = req.body;
+    const accountToSave = {
+        name: body.name,
+        email: body.email,
+        passowrd: body.passoword,
+        document: body.document,
+        accountId: randomUUID(),
+    };
+    accounts.push(accountToSave);
+    res.json({ accountId: accountToSave.accountId });
+});
+
+app.get('/accounts/:accountId', async(req: Request, res: Response) => {
+    const accountId = req.params.accountId
+    const account = accounts.find((item: any) => item.accountId === accountId);
+    res.json(account)
 });
 
 app.listen(3025);
