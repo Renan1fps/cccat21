ec8a9281d97acab99d56e908923e0928f899e6db
Renan1fps <renan.sergipe68@gmail.com>
2025-07-21 08:52:14 -0300

    test: ensures that the account was saved correctly and returned

--- src/test/signup.test.ts
+++ src/test/signup.test.ts
@@ -3,15 +3,19 @@ import axios from "axios";
 describe('Signup [integration]', () => {
 
     test('Must create an account correctly', async() => {
-        const inputCreateAccount = {
+        const inputSignup = {
             name: 'Jhon Doe',
             email: 'jhondoe@gmail.com',
             document: '60993883893',
             password: 'Test.1234',
         }
-        const responseSignup = await axios.post('http://localhost:3025/signup', inputCreateAccount);
+        const responseSignup = await axios.post('http://localhost:3025/signup', inputSignup);
         const outputSignup = responseSignup.data;
-
         expect(outputSignup.accountId).toBeDefined();
+        const responseGetAccount = await axios.get(`http://localhost:3025/accounts/${outputSignup.accountId}`);
+        const outputGetAccount = responseGetAccount.data;
+        expect(outputGetAccount.name).toBe(inputSignup.name);
+        expect(outputGetAccount.email).toBe(inputSignup.email);
+        expect(outputGetAccount.document).toBe(inputSignup.document);
     });
 });
